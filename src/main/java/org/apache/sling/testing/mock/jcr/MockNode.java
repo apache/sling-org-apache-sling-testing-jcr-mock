622687f8b2a7d17bf3d8ac948579ff46aa6be186
Dumitru Catalina <52375949+catalinadumitruu@users.noreply.github.com>
2023-02-21 21:41:14 +0200

    Update MockNode.java

--- src/main/java/org/apache/sling/testing/mock/jcr/MockNode.java
+++ src/main/java/org/apache/sling/testing/mock/jcr/MockNode.java
@@ -31,6 +31,7 @@ import javax.jcr.Item;
 import javax.jcr.ItemNotFoundException;
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.Property;
 import javax.jcr.PropertyIterator;
 import javax.jcr.RangeIterator;
